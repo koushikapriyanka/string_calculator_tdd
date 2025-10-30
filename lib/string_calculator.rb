83b922a71d31b4c5da5e16de3e455caf4e61874f
koushikapriyanka <priyankapaidi@gmail.com>
2025-10-30 07:07:12 +0000

    handled custom delimiters defined with //

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,6 +2,15 @@ class StringCalculator
     def add(numbers)
       return 0 if numbers.empty?
       nums = numbers.split(/,|\n/).map(&:to_i)
+      delimiter = /,|\n/
+      if numbers.start_with?("//")
+        parts = numbers.split("\n", 2)
+        custom_delimiter = parts[0][2..]
+        delimiter = Regexp.escape(custom_delimiter)
+        numbers = parts[1]
+      end
+
+      nums = numbers.split(/#{delimiter}/).map(&:to_i)
       nums.sum  
     end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -22,6 +22,10 @@ RSpec.describe StringCalculator do
     
     it "handles newlines as delimiters" do
       expect(calc.add("1\n2,3")).to eq(6)
+    end     
+    
+    it "supports custom delimiter" do
+      expect(calc.add("//;\n1;2")).to eq(3)
     end      
   end
 end
